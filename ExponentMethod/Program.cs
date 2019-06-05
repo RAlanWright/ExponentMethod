327e0ab64c6c86048b242506140e4c461e5beef3
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-05 06:52:31 -0500

    Added method; Allows for both positive and negative exponents; Returns a double; Currently only set up to receive baseNum and powNum from editor.

--- ExponentMethod/Program.cs
+++ ExponentMethod/Program.cs
@@ -10,6 +10,33 @@ namespace ExponentMethod
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(GetPow(8, -1));
+
+
+            Console.ReadLine();
         }
+
+        static double GetPow(int baseNum, int powNum)
+        {
+            double result = 1;
+            
+            if (powNum > 0)
+            {
+                for (int i = 0; i < powNum; i++)
+                {
+                    result = result * baseNum;
+                }
+            } else if (powNum < 0)
+            {
+                for (int i = 0; i > powNum; i--)
+                {
+                    result = result / baseNum;
+                }
+            }
+            
+
+            return result;
+        }
+
     }
 }
